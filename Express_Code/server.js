ffeef829be1c2a3a7d22de5c9bbeb0bbf8c4a9ee
Vasudev290 <vasukesavulu@gmail.com>
2025-04-13 18:56:38 +0530

    router

--- Express_Code/server.js
+++ Express_Code/server.js
@@ -8,6 +8,19 @@ app.get('/get', (req, res) => {
     //res.end()
 });
 
+app.get('/get-all-users', (req, res) => {
+    res.send("List of all Users")
+});
+
+app.get('/get-user-details', (req, res) => {
+    const userDetails = {
+        userId: 101,
+        userName: "Vasu Devu",
+        userEmail:"vasu@gmail.com",
+        userCity: "Bangalore"
+    }
+    res.send(userDetails)
+})
 
 app.listen(8000, () => {
     console.log("Server Started Connected Successfull!")
